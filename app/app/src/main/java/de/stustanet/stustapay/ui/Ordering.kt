f48544c27bd8ee8e96853b2fec79e10011aa39df
Alessandro Wollek <alessandro.wollek@tum.de>
2023-01-22 19:08:54 +0100

    app: sample order buttons

--- app/app/src/main/java/de/stustanet/stustapay/ui/Ordering.kt
+++ app/app/src/main/java/de/stustanet/stustapay/ui/Ordering.kt
@@ -1,50 +1,70 @@
 package de.stustanet.stustapay.ui
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-
 
 @Composable
-fun OrderItem(label: String) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 24.dp, vertical = 10.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            modifier = Modifier.padding(start = 16.dp),
-            text = label,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Medium
-        )
+private fun OrderItem(name: String, price: String, amount: String) = Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+) {
+    Text(text = price.plus(" x ").plus(amount),
+            modifier = Modifier.fillMaxWidth(0.3f))
+    Box(modifier = Modifier.fillMaxWidth(0.9f)) {
+        Row(    horizontalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.fillMaxWidth(0.7f)
+            ) {
+                Text(text = name)
+            }
+            Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "-")
+            }
+        }
     }
+
 }
 
 @Preview
 @Composable
 fun OrderView() {
-    LazyColumn {
-        item {
-            Text(text = "First item")
-        }
+    val state = rememberScaffoldState()
 
-        items(50) { index ->
-            OrderItem(label = "item $index")
-        }
+    Scaffold(
+            scaffoldState = state,
 
-        item {
-            Text(text = "Last item")
-        }
-    }
+            content = {
+                LazyColumn {
+                    item{ OrderItem(name = "Bier", amount = "2", price = "2")}
+                    item{ OrderItem(name = "Maß", amount = "1", price = "4")}
+                    item{ OrderItem(name = "Radler", amount = "1", price = "1,5")}
+                    item{ OrderItem(name = "Spezi", amount = "0", price = "")}
+                    item{ OrderItem(name = "Weißbier", amount = "0", price = "")}
+                    item{ OrderItem(name = "Pfand zurück", amount = "0", price = "")}
+                }
+            },
+
+            bottomBar = {
+                Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth(0.45f)) {
+                        Text(text = "❌")
+                    }
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth(0.9f)) {
+                        Text(text = "✓")
+                    }
+                }
+            }
+    )
+
 }
