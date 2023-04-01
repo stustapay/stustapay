925feee5707fa0407adabf7d382642fe2de7ad79
TechnoElf <technoelf@undertheprinter.com>
2023-04-02 01:20:02 +0200

    app: refactor deposit view

---
+++ app/app/src/main/java/de/stustanet/stustapay/ui/deposit/DepositCash.kt
@@ -0,0 +1,68 @@
+package de.stustanet.stustapay.ui.deposit
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import de.stustanet.stustapay.ui.chipscan.ChipScanDialog
+import de.stustanet.stustapay.ui.nav.navigateTo
+
+@Composable
+fun DepositCash(nav: NavHostController, viewModel: DepositViewModel) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val haptic = LocalHapticFeedback.current
+    var scanning by remember { mutableStateOf(false) }
+
+    Scaffold(
+        content = { paddingValues ->
+            Box(
+                modifier = Modifier.fillMaxSize()
+                    .padding(bottom = paddingValues.calculateBottomPadding()),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("%.2f€".format(uiState.currentAmount.toFloat() / 100), fontSize = 48.sp)
+                    Text("received", fontSize = 48.sp)
+                }
+            }
+        },
+        bottomBar = {
+            Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+                Button(
+                    onClick = {
+                        nav.navigateTo("method")
+                        haptic.performHapticFeedback(HapticFeedbackType.LongPress)
+                    },
+                    modifier = Modifier.fillMaxWidth(0.5f).height(70.dp).padding(10.dp)
+                ) {
+                    Text(text = "❌")
+                }
+                Button(
+                    onClick = {
+                        scanning = true
+                        haptic.performHapticFeedback(HapticFeedbackType.LongPress)
+                    },
+                    modifier = Modifier.fillMaxWidth().height(70.dp).padding(10.dp)
+                ) {
+                    Text(text = "✓")
+                }
+            }
+        }
+    )
+
+    if (scanning) {
+        ChipScanDialog(
+            onScan = { nav.navigateTo("success") },
+            onDismissRequest = { scanning = false }
+        )
+    }
+}
--- app/app/src/main/java/de/stustanet/stustapay/ui/deposit/DepositView.kt
+++ app/app/src/main/java/de/stustanet/stustapay/ui/deposit/DepositView.kt
@@ -1,325 +1,35 @@
 package de.stustanet.stustapay.ui.deposit
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.hapticfeedback.HapticFeedbackType
-import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import de.stustanet.stustapay.ui.chipscan.ChipScanDialog
 
 @Preview
 @Composable
 fun DepositView(viewModel: DepositViewModel = hiltViewModel()) {
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val nav = rememberNavController()
-    val haptic = LocalHapticFeedback.current
-    var scanning by remember { mutableStateOf(false) }
-
-    val amount = uiState.currentAmount.toFloat() / 100
 
     NavHost(navController = nav, startDestination = "main") {
         composable("main") {
-            Scaffold(
-                content = { paddingValues ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(bottom = paddingValues.calculateBottomPadding()),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Box(
-                            modifier = Modifier.height(200.dp),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = "$amount€", fontSize = 72.sp)
-                        }
-                        DepositKeyboard(
-                            onDigit = { viewModel.inputDigit(it) },
-                            onClear = { viewModel.clear() }
-                        )
-                    }
-                },
-                bottomBar = {
-                    Row() {
-                        Button(
-                            onClick = {
-                                scanning = true
-                                haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                            },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(70.dp)
-                                .padding(10.dp)
-                        ) {
-                            Text(text = "✓")
-                        }
-                    }
-                }
-            )
-
-            if (scanning) {
-                ChipScanDialog(
-                    onScan = { uid ->
-                        // TODO: Connect to DB
-                        if (amount < 10) {
-                            nav.navigate("success") { launchSingleTop = true }
-                        } else {
-                            nav.navigate("failure") { launchSingleTop = true }
-                        }
-                    },
-                    onDismissRequest = {
-                        scanning = false
-                    }
-                )
-            }
+            DepositMain(nav, viewModel)
+        }
+        composable("method") {
+            DepositMethod(nav, viewModel)
+        }
+        composable("cash") {
+            DepositCash(nav, viewModel)
+        }
+        composable("card") {
+            DepositCard(nav, viewModel)
         }
         composable("success") {
-            Scaffold(
-                content = {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(bottom = it.calculateBottomPadding()),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(text = "$amount€\ndeposited", fontSize = 48.sp)
-                    }
-                },
-                bottomBar = {
-                    Button(
-                        onClick = {
-                            viewModel.clear()
-                            nav.navigate("main") { launchSingleTop = true }
-                            haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(70.dp)
-                            .padding(10.dp)
-                    ) {
-                        Text(text = "Next")
-                    }
-                }
-            )
+            DepositSuccess(nav, viewModel)
         }
         composable("failure") {
-            Scaffold(
-                content = {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(bottom = it.calculateBottomPadding()),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(text = "Transaction Failed", fontSize = 48.sp)
-                    }
-                },
-                bottomBar = {
-                    Button(
-                        onClick = {
-                            viewModel.clear()
-                            nav.navigate("main") { launchSingleTop = true }
-                            haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(70.dp)
-                            .padding(10.dp)
-                    ) {
-                        Text(text = "Next")
-                    }
-                }
-            )
-        }
-    }
-}
-
-@Composable
-fun DepositKeyboard(onDigit: (UInt) -> Unit, onClear: () -> Unit) {
-    val haptic = LocalHapticFeedback.current
-
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Row (
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(0.33f)
-                    .height(90.dp)
-                    .padding(5.dp),
-                onClick = {
-                    onDigit(1u)
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                }
-            ) {
-                Text("1", fontSize = 24.sp)
-            }
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .height(90.dp)
-                    .padding(5.dp),
-                onClick = {
-                    onDigit(2u)
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                }
-            ) {
-                Text("2", fontSize = 24.sp)
-            }
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(1f)
-                    .height(90.dp)
-                    .padding(5.dp),
-                onClick = {
-                    onDigit(3u)
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                }
-            ) {
-                Text("3", fontSize = 24.sp)
-            }
-        }
-        Row (
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(0.33f)
-                    .height(90.dp)
-                    .padding(5.dp),
-                onClick = {
-                    onDigit(4u)
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                }
-            ) {
-                Text("4", fontSize = 24.sp)
-            }
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .height(90.dp)
-                    .padding(5.dp),
-                onClick = {
-                    onDigit(5u)
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                }
-            ) {
-                Text("5", fontSize = 24.sp)
-            }
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(1f)
-                    .height(90.dp)
-                    .padding(5.dp),
-                onClick = {
-                    onDigit(6u)
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                }
-            ) {
-                Text("6", fontSize = 24.sp)
-            }
-        }
-        Row (
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(0.33f)
-                    .height(90.dp)
-                    .padding(5.dp),
-                onClick = {
-                    onDigit(7u)
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                }
-            ) {
-                Text("7", fontSize = 24.sp)
-            }
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .height(90.dp)
-                    .padding(5.dp),
-                onClick = {
-                    onDigit(8u)
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                }
-            ) {
-                Text("8", fontSize = 24.sp)
-            }
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(1f)
-                    .height(90.dp)
-                    .padding(5.dp),
-                onClick = {
-                    onDigit(9u)
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                }
-            ) {
-                Text("9", fontSize = 24.sp)
-            }
-        }
-        Row (
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(0.33f)
-                    .height(90.dp)
-                    .padding(5.dp),
-                onClick = {
-                    onDigit(0u)
-                    onDigit(0u)
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                }
-            ) {
-                Text("00", fontSize = 24.sp)
-            }
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .height(90.dp)
-                    .padding(5.dp),
-                onClick = {
-                    onDigit(0u)
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                }
-            ) {
-                Text("0", fontSize = 24.sp)
-            }
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(1f)
-                    .height(90.dp)
-                    .padding(5.dp),
-                onClick = {
-                    onClear()
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                }
-            ) {
-                Text("❌", fontSize = 24.sp)
-            }
+            DepositFailure(nav, viewModel)
         }
     }
 }
