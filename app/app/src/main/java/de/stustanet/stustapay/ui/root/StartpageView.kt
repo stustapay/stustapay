e34f6c2ce814be55133c3e5bb1de8c780ec20d32
Janis Heims <janis.heims@stusta.de>
2023-04-26 01:01:51 +0200

    app: hide views on start page based on user and terminal privileges

--- app/app/src/main/java/de/stustanet/stustapay/ui/root/StartpageView.kt
+++ app/app/src/main/java/de/stustanet/stustapay/ui/root/StartpageView.kt
@@ -11,6 +11,8 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -19,6 +21,11 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import de.stustanet.stustapay.model.Access
+import de.stustanet.stustapay.model.TerminalConfig
+import de.stustanet.stustapay.model.User
 import de.stustanet.stustapay.ui.nav.NavDest
 
 data class NavMenuItem(
@@ -26,7 +33,9 @@ data class NavMenuItem(
     val label: String,
     val navDestination: NavDest? = null,
     val isUnread: Boolean = false,
-    val dividePrevious: Boolean = false
+    val dividePrevious: Boolean = false,
+    val canAccess: (User, TerminalConfig) -> Boolean = { _, _ -> false },
+    val alwaysShow: Boolean = false
 )
 
 @Composable
@@ -38,6 +47,7 @@ private fun getNavItems(): List<NavMenuItem> {
             icon = Icons.Filled.ShoppingCart,
             label = "Process Orders",
             navDestination = RootNavDests.ordering,
+            canAccess = { u, _ -> Access.canSell(u) }
         )
     )
     itemsList.add(
@@ -45,6 +55,7 @@ private fun getNavItems(): List<NavMenuItem> {
             icon = Icons.Filled.Add,
             label = "Process Deposits",
             navDestination = RootNavDests.deposit,
+            canAccess = { _, t -> Access.canTopUp(t) }
         )
     )
     itemsList.add(
@@ -52,20 +63,23 @@ private fun getNavItems(): List<NavMenuItem> {
             icon = Icons.Filled.Info,
             label = "Account Status",
             navDestination = RootNavDests.status,
+            canAccess = { u, _ -> Access.canSell(u) }
         )
     )
     itemsList.add(
         NavMenuItem(
             icon = Icons.Filled.List,
             label = "Transaction History",
-            navDestination = RootNavDests.history
+            navDestination = RootNavDests.history,
+            canAccess = { u, _ -> Access.canSell(u) }
         )
     )
     itemsList.add(
         NavMenuItem(
             icon = Icons.Filled.Person,
             navDestination = RootNavDests.user,
-            label = "User"
+            label = "User",
+            alwaysShow = true
         )
     )
     itemsList.add(
@@ -74,13 +88,15 @@ private fun getNavItems(): List<NavMenuItem> {
             label = "Settings",
             navDestination = RootNavDests.settings,
             dividePrevious = true,
+            alwaysShow = true
         )
     )
     itemsList.add(
         NavMenuItem(
             icon = Icons.Filled.Send,
             label = "Development",
-            navDestination = RootNavDests.development
+            navDestination = RootNavDests.development,
+            canAccess = { u, _ -> Access.canHackTheSystem(u) }
         )
     )
 
@@ -91,10 +107,15 @@ private fun getNavItems(): List<NavMenuItem> {
 @Composable
 fun StartpageView(
     navigateTo: (NavDest) -> Unit = {},
+    viewModel: StartpageViewModel = hiltViewModel()
 ) {
-
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val navItems = getNavItems()
     val gradientColors = listOf(MaterialTheme.colors.background, MaterialTheme.colors.onSecondary)
+
+    LaunchedEffect(null) {
+        viewModel.fetchAccessData()
+    }
 
     Column(
         modifier = Modifier
@@ -116,7 +137,9 @@ fun StartpageView(
                 Divider()
             }
 
-            StartpageEntry(item = item, navigateTo = navigateTo)
+            if (item.alwaysShow || uiState.checkAccess(item.canAccess)) {
+                StartpageEntry(item = item, navigateTo = navigateTo)
+            }
         }
     }
 }
