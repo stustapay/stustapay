35d90c8bffb5553a410b19419d1d2489881c47ba
Markus Hefele <markus.hefele@stusta.de>
2023-05-08 22:04:03 +0200

    TSE: more simulator features

--- stustapay/tse/simulator.py
+++ stustapay/tse/simulator.py
@@ -8,13 +8,16 @@ class Simulator(SubCommand):
     def argparse_register(subparser):
         subparser.add_argument("--host", default="::1")
         subparser.add_argument("-p", "--port", type=int, default=10001)
+        subparser.add_argument("-d", "--delay", type=float, default=0.25)
+        subparser.add_argument("-f", "--fast", action="store_true", default=False)
+        subparser.add_argument("-r", "--even_more_realistic", action="store_true", default=False)
 
     def __init__(self, args, config: Config, **rest):
         del rest  # unused
 
         self.cfg = config
 
-        self.tse_sim = WebsocketInterface(args.host, args.port)
+        self.tse_sim = WebsocketInterface(args.host, args.port, args.delay, args.fast, args.even_more_realistic)
 
     async def run(self):
         await self.tse_sim.run()
