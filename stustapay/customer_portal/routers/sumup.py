33a73d883757f9ded6ad9b7d864992dc346e4ded
Tobias Juelg <tobias.juelg@tum.de>
2023-05-28 17:49:19 +0200

    feat(cp-b): added check checkout api call
    updated calls to only use checkout id

--- stustapay/customer_portal/routers/sumup.py
+++ stustapay/customer_portal/routers/sumup.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, status
 
 from stustapay.core.http.auth_customer import CurrentAuthToken
 from stustapay.core.http.context import ContextCustomerService
+from stustapay.core.schema.customer import SumupCheckoutStatus
 from stustapay.core.util import BaseModel
 
 router = APIRouter(
@@ -22,7 +23,16 @@ class CreateCheckoutPayload(BaseModel):
 
 
 class CreateCheckoutResponse(BaseModel):
-    checkout_reference: uuid.UUID
+    checkout_id: str
+
+
+class CheckCheckoutPayload(BaseModel):
+    checkout_id: str
+
+
+class CheckCheckoutResponse(BaseModel):
+    status: SumupCheckoutStatus
+    checkout_id: str
 
 
 @router.post("/create-checkout", summary="initiate customer checkout", response_model=CreateCheckoutResponse)
@@ -33,5 +43,15 @@ async def create_checkout(
 ):
     # TODO: change this back!!
     # checkout = await customer_service.sumup.create_checkout(token=token, amount=payload.amount)
-    # return CreateCheckoutResponse(checkout_reference=checkout.checkout_reference)
-    return CreateCheckoutResponse(checkout_reference=uuid.uuid4())
+    # return CreateCheckoutResponse(checkout_id=checkout.id)
+    return CreateCheckoutResponse(checkout_id=str(uuid.uuid4()))
+
+
+@router.post("/check-checkout", summary="after payment check checkout state", response_model=CheckCheckoutResponse)
+async def create_checkout(
+    token: CurrentAuthToken,
+    customer_service: ContextCustomerService,
+    payload: CheckCheckoutPayload,
+):
+    update = await customer_service.sumup.check_checkout(token=token, checkout_id=payload.checkout_id)
+    return CheckCheckoutResponse(status=update, checkout_id=payload.checkout_id)
