bdf6ef23041dc83463fa8bab5da3251d66489fc4
Michael Loipführer <michael.loipfuehrer@stusta.de>
2023-05-29 17:36:57 +0200

    feat(core): add populate script for tag data reading

--- stustapay/core/database.py
+++ stustapay/core/database.py
@@ -27,7 +27,7 @@ logger = logging.getLogger(__name__)
 REVISION_VERSION_RE = re.compile(r"^-- revision: (?P<version>\w+)$")
 REVISION_REQUIRES_RE = re.compile(r"^-- requires: (?P<version>\w+)$")
 REVISION_TABLE = "schema_revision"
-CURRENT_REVISION = "b71733fb"
+CURRENT_REVISION = "66c577ad"
 
 
 class DatabaseManage(subcommand.SubCommand):
---
+++ stustapay/core/schema/db/0002-user-tag-secrets-comment.sql
@@ -0,0 +1,4 @@
+-- revision: 66c577ad
+-- requires: b71733fb
+
+alter table user_tag_secret add column description text;
