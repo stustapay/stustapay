cbc006648e0b4300ed7cd09a72d2f038f1d5039b
Jonas Jelten <jj@sft.lol>
2023-06-04 14:43:25 +0200

    app: align account info scan in center

--- app/app/src/main/java/de/stustanet/stustapay/ui/account/AccountScan.kt
+++ app/app/src/main/java/de/stustanet/stustapay/ui/account/AccountScan.kt
@@ -1,10 +1,10 @@
 package de.stustanet.stustapay.ui.account
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import de.stustanet.stustapay.model.UserTag
@@ -12,7 +12,12 @@ import de.stustanet.stustapay.ui.chipscan.NfcScanCard
 
 @Composable
 fun AccountScan(onScan: (UserTag) -> Unit) {
-    Box(modifier = Modifier.padding(20.dp)) {
+    Box(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
         NfcScanCard(
             onScan = onScan,
             keepScanning = true
