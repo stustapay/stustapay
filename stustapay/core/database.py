a12bd322b48f27d8b331e36a3b3a9d6002ddf2a6
Tobias Juelg <tobias.juelg@tum.de>
2023-06-27 21:21:54 +0200

    Fix revision

--- stustapay/core/database.py
+++ stustapay/core/database.py
@@ -28,7 +28,7 @@ logger = logging.getLogger(__name__)
 REVISION_VERSION_RE = re.compile(r"^-- revision: (?P<version>\w+)$")
 REVISION_REQUIRES_RE = re.compile(r"^-- requires: (?P<version>\w+)$")
 REVISION_TABLE = "schema_revision"
-CURRENT_REVISION = "c5c4ae30"
+CURRENT_REVISION = "2a15b453"
 
 
 class DatabaseManage(subcommand.SubCommand):
