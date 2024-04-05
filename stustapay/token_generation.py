56a788d09f521323a0e4a26b7cfe0ae1ff6c3343
Jonas Jelten <jj@sft.lol>
2024-04-05 17:55:10 +0200

    misc: new nfc token generation

--- stustapay/token_generation.py
+++ stustapay/token_generation.py
@@ -1,22 +1,19 @@
-import binascii
-import random
+import secrets
 from textwrap import wrap
 
 
 def generate_nfc(count: int):
-    print("index,id,pin")
+    print("index,pin")
     for i in range(count):
-        chipid = binascii.hexlify(random.randbytes(8)).decode()
+        # so we have 36520347436056576 pins
+        pin = "".join((secrets.choice("ACDEFHJKLMNPQRTUVWXY3479") for _ in range(12)))
 
-        # so we have 1291467969 pins
-        pin = "".join(random.choices("abcdefghjkmnpqrstuvwxyz0123456789", k=6))
-
-        print(f"{i},{chipid},{pin}")
+        print(f"{i},{pin}")
 
 
 def generate_key():
-    key0 = binascii.hexlify(random.randbytes(16)).decode()
-    key1 = binascii.hexlify(random.randbytes(16)).decode()
+    key0 = secrets.token_hex(16)
+    key1 = secrets.token_hex(16)
 
     # split in groups
     def pretty(key):
@@ -30,7 +27,7 @@ def generate_key():
     print("```")
     print()
     print("So:")
-    print(f"- `key0[0]  == {key0[0:2]}`")
-    print(f"- `key0[15] == {key0[15*2:15*2+2]}`")
-    print(f"- `key1[0]  == {key1[0:2]}`")
-    print(f"- `key1[15] == {key1[15*2:15*2+2]}`")
+    print(f"- `key0[0]  == 0x{key0[0:2]}`")
+    print(f"- `key0[15] == 0x{key0[15*2:15*2+2]}`")
+    print(f"- `key1[0]  == 0x{key1[0:2]}`")
+    print(f"- `key1[15] == 0x{key1[15*2:15*2+2]}`")
