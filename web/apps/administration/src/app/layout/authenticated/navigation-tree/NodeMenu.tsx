6028f81c5ff99790eeffca2161ee65d14373ca8e
Michael Loipführer <michael.loipfuehrer@stusta.de>
2024-05-09 17:50:52 +0200

    fix(admin): properly highlight active routes in sidebar menu

--- web/apps/administration/src/app/layout/authenticated/navigation-tree/NodeMenu.tsx
+++ web/apps/administration/src/app/layout/authenticated/navigation-tree/NodeMenu.tsx
@@ -1,4 +1,4 @@
-import { NodeSeenByUser } from "@/api";
+import { Node, NodeSeenByUser, ObjectType, Privilege } from "@/api";
 import {
   AccountBalance as AccountBalanceIcon,
   ConfirmationNumber as ConfirmationNumberIcon,
@@ -11,7 +11,6 @@ import {
   Smartphone as SmartphoneIcon,
 } from "@mui/icons-material";
 import * as React from "react";
-import { useTranslation } from "react-i18next";
 import { NavigationTreeItem } from "./NavigationTreeItem";
 import {
   CashierRoutes,
@@ -26,106 +25,152 @@ import {
   UserTagRoutes,
   UserToRoleRoutes,
 } from "@/app/routes";
+import { i18n } from "@/i18n";
+
+type NodeMenuItem = {
+  route: (node: Node) => string;
+  icon: React.FC;
+  label: string;
+  requiresEvent?: boolean;
+  requiredPrivileges?: Privilege[];
+  requiresOneOfObjectType?: ObjectType[];
+  additionalRequirements?: (node: Node) => boolean;
+};
+
+export const nodeMenuEntryDefinitions: NodeMenuItem[] = [
+  {
+    route: (node) => UserRoutes.list(node.id),
+    label: i18n.t("users"),
+    icon: PersonIcon,
+    requiresOneOfObjectType: ["user", "user_role"],
+  },
+  {
+    route: (node) => CashierRoutes.list(node.id),
+    label: i18n.t("cashiers"),
+    icon: PersonIcon,
+    requiresOneOfObjectType: ["user", "user_role"],
+    requiresEvent: true,
+  },
+  {
+    route: (node) => UserToRoleRoutes.list(node.id),
+    label: i18n.t("userToRoles"),
+    icon: PersonIcon,
+  },
+  {
+    route: (node) => ProductRoutes.list(node.id),
+    label: i18n.t("products"),
+    icon: ShoppingCartIcon,
+    requiresOneOfObjectType: ["product"],
+  },
+  {
+    route: (node) => TicketRoutes.list(node.id),
+    label: i18n.t("tickets"),
+    icon: ConfirmationNumberIcon,
+    requiresOneOfObjectType: ["ticket"],
+  },
+  {
+    route: (node) => TerminalRoutes.list(node.id),
+    label: i18n.t("terminal.terminals"),
+    icon: SmartphoneIcon,
+    requiresOneOfObjectType: ["terminal"],
+  },
+  {
+    route: (node) => TillRoutes.list(node.id),
+    label: i18n.t("tills"),
+    icon: PointOfSaleIcon,
+    requiresOneOfObjectType: ["till"],
+  },
+  {
+    route: (node) => SumUpTransactionRoutes.list(node.id),
+    label: i18n.t("sumup.sumup"),
+    icon: PaymentIcon,
+    requiresEvent: true,
+    requiredPrivileges: ["node_administration"],
+    additionalRequirements: (node) =>
+      node.event != null && (node.event.sumup_payment_enabled || node.event.sumup_topup_enabled),
+  },
+  {
+    route: (node) => CustomerRoutes.list(node.id),
+    label: i18n.t("customer.customers"),
+    icon: AccountBalanceIcon,
+    requiresEvent: true,
+    requiredPrivileges: ["node_administration"],
+  },
+  {
+    route: (node) => UserTagRoutes.list(node.id),
+    label: i18n.t("userTag.userTags"),
+    icon: NfcIcon,
+    requiresEvent: true,
+    requiresOneOfObjectType: ["user_tag"],
+    requiredPrivileges: ["node_administration"],
+  },
+  {
+    route: (node) => TseRoutes.list(node.id),
+    label: i18n.t("tse.tses"),
+    icon: ShieldIcon,
+    requiresEvent: true,
+    requiresOneOfObjectType: ["tse"],
+    requiredPrivileges: ["node_administration"],
+  },
+];
+
+export const isMenuEntryValidAtNode = (entry: NodeMenuItem, node: NodeSeenByUser) => {
+  const isEvent = node.event != null;
+
+  if (!isEvent && entry.requiresEvent === true) {
+    return false;
+  }
+
+  if (
+    entry.requiresOneOfObjectType != null &&
+    entry.requiresOneOfObjectType.length > 0 &&
+    entry.requiresOneOfObjectType.every((val) => node.computed_forbidden_objects_at_node.includes(val))
+  ) {
+    return false;
+  }
+
+  if (entry.additionalRequirements != null && !entry.additionalRequirements(node)) {
+    return false;
+  }
+
+  if (
+    entry.requiredPrivileges != null &&
+    entry.requiredPrivileges.length > 0 &&
+    !entry.requiredPrivileges.every((privilege) => node.privileges_at_node.includes(privilege))
+  ) {
+    return false;
+  }
+  return true;
+};
 
 export interface NodeMenuProps {
   node: NodeSeenByUser;
 }
 
 export const NodeMenu: React.FC<NodeMenuProps> = React.memo(({ node }) => {
-  const { t } = useTranslation();
-  const isEvent = node.event != null;
-
   if (node.computed_forbidden_objects_at_node === undefined) {
     return null;
   }
 
-  const items: React.ReactElement[] = [];
+  const renderedItems = [];
 
-  if (
-    !node.computed_forbidden_objects_at_node.includes("user") ||
-    !node.computed_forbidden_objects_at_node.includes("user_role")
-  ) {
-    const id = UserRoutes.list(node.id);
-    items.push(<NavigationTreeItem key={id} nodeId={id} to={id} labelText={t("users")} labelIcon={PersonIcon} />);
-    if (node.event_node_id != null) {
-      const cashierId = CashierRoutes.list(node.id);
-      items.push(
-        <NavigationTreeItem
-          key={cashierId}
-          nodeId={cashierId}
-          to={cashierId}
-          labelText={t("cashiers")}
-          labelIcon={PersonIcon}
-        />
-      );
+  for (const menuDefinition of nodeMenuEntryDefinitions) {
+    if (!isMenuEntryValidAtNode(menuDefinition, node)) {
+      continue;
     }
-  }
 
-  {
-    const id = UserToRoleRoutes.list(node.id);
-    items.push(<NavigationTreeItem key={id} nodeId={id} to={id} labelText={t("userToRoles")} labelIcon={PersonIcon} />);
-  }
+    const id = menuDefinition.route(node);
 
-  if (!node.computed_forbidden_objects_at_node.includes("product")) {
-    const id = ProductRoutes.list(node.id);
-    items.push(
-      <NavigationTreeItem key={id} nodeId={id} to={id} labelText={t("products")} labelIcon={ShoppingCartIcon} />
-    );
-  }
-  if (!node.computed_forbidden_objects_at_node.includes("ticket")) {
-    const id = TicketRoutes.list(node.id);
-    items.push(
-      <NavigationTreeItem key={id} nodeId={id} to={id} labelText={t("tickets")} labelIcon={ConfirmationNumberIcon} />
-    );
-  }
-  if (!node.computed_forbidden_objects_at_node.includes("till")) {
-    const id = TillRoutes.list(node.id);
-    items.push(<NavigationTreeItem key={id} nodeId={id} to={id} labelText={t("tills")} labelIcon={PointOfSaleIcon} />);
-  }
-  if (!node.computed_forbidden_objects_at_node.includes("terminal")) {
-    const id = TerminalRoutes.list(node.id);
-    items.push(
-      <NavigationTreeItem key={id} nodeId={id} to={id} labelText={t("terminal.terminals")} labelIcon={SmartphoneIcon} />
-    );
-  }
-  if (
-    node.event != null &&
-    node.privileges_at_node.includes("node_administration") &&
-    (node.event.sumup_payment_enabled || node.event.sumup_topup_enabled)
-  ) {
-    const id = SumUpTransactionRoutes.list(node.id);
-    items.push(
-      <NavigationTreeItem key={id} nodeId={id} to={id} labelText={t("sumup.sumup")} labelIcon={PaymentIcon} />
-    );
-  }
-
-  if (isEvent && node.privileges_at_node.includes("node_administration")) {
-    const id = CustomerRoutes.list(node.id);
-    items.push(
+    renderedItems.push(
       <NavigationTreeItem
         key={id}
         nodeId={id}
         to={id}
-        labelText={t("customer.customers")}
-        labelIcon={AccountBalanceIcon}
+        labelText={menuDefinition.label}
+        labelIcon={menuDefinition.icon}
       />
     );
   }
 
-  if (!node.computed_forbidden_objects_at_node.includes("user_tag")) {
-    const id = UserTagRoutes.list(node.id);
-    items.push(
-      <NavigationTreeItem key={id} nodeId={id} to={id} labelText={t("userTag.userTags")} labelIcon={NfcIcon} />
-    );
-  }
-
-  if (
-    !node.computed_forbidden_objects_at_node.includes("tse") &&
-    node.privileges_at_node.includes("node_administration")
-  ) {
-    const id = TseRoutes.list(node.id);
-    items.push(<NavigationTreeItem key={id} nodeId={id} to={id} labelText={t("tse.tses")} labelIcon={ShieldIcon} />);
-  }
-
-  return <>{items}</>;
+  return renderedItems;
 });
