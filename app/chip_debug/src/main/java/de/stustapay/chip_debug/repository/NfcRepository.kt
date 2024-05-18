9821c300680377e2b4ff33166ce57ce2584e4992
Jonas Jelten <jj@sft.lol>
2024-05-18 16:02:07 +0200

    app: remove obsolete nfc repo content

--- app/chip_debug/src/main/java/de/stustapay/chip_debug/repository/NfcRepository.kt
+++ app/chip_debug/src/main/java/de/stustapay/chip_debug/repository/NfcRepository.kt
@@ -3,20 +3,13 @@ package de.stustapay.chip_debug.repository
 import de.stustapay.libssp.model.NfcScanFailure
 import de.stustapay.libssp.model.NfcScanRequest
 import de.stustapay.libssp.model.NfcScanResult
-import de.stustapay.api.models.UserTagSecret
 import de.stustapay.libssp.nfc.NfcDataSource
 import de.stustapay.libssp.util.BitVector
-import de.stustapay.libssp.util.bv
 import de.stustapay.libssp.util.decodeHex
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 import javax.inject.Singleton
 
-sealed interface ReadMode {
-    object Fast : ReadMode
-    data class Full(val auth: Boolean, val cmac: Boolean) : ReadMode
-}
 
 @Singleton
 class NfcRepository @Inject constructor(
@@ -27,11 +20,6 @@ class NfcRepository @Inject constructor(
         MutableStateFlow<BitVector?>("00102030405060708090a0b0c0d0e0f0".decodeHex())
     private val dataProtKey =
         MutableStateFlow<BitVector?>("000102030405060708090a0b0c0d0e0f".decodeHex())
-
-    fun setTagKeys(secrets: UserTagSecret) {
-        uidRetrKey.update { secrets.key1.decodeHex() }
-        dataProtKey.update { secrets.key0.decodeHex() }
-    }
 
     suspend fun read(): NfcScanResult {
         return nfcDataSource.scan(
