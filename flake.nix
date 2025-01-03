2ede191e910d6030a241258b42b964bf107034db
TechnoElf <technoelf@undertheprinter.com>
2025-01-03 15:36:47 +0100

    chore: update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -127,6 +127,8 @@
           "uvicorn"
           "pydantic"
           "python-multipart"
+          "passlib"
+          "asyncpg"
         ];
       };
 
