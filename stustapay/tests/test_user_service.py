5f97ea0e265f0b4e57c6e14a36b124307a51284b
Jonas Jelten <jj@sft.lol>
2025-03-02 16:54:29 +0100

    test: change test password

--- stustapay/tests/test_user_service.py
+++ stustapay/tests/test_user_service.py
@@ -8,7 +8,7 @@ from stustapay.core.service.user import UserService
 async def test_change_password(user_service: UserService, event_admin_user, event_admin_token: str):
     usr, password = event_admin_user
     with pytest.raises(AccessDenied):  # test with invalid password
-        await user_service.change_password(token=event_admin_token, old_password="foobar", new_password="asdf")
-    await user_service.change_password(token=event_admin_token, old_password=password, new_password="asdf")
+        await user_service.change_password(token=event_admin_token, old_password="foobar", new_password="rofl")
+    await user_service.change_password(token=event_admin_token, old_password=password, new_password="rofl")
 
-    await user_service.login_user(username=usr.login, password="asdf")
+    await user_service.login_user(username=usr.login, password="rofl")
