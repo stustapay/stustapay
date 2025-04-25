a94e09e27e67352f4af64beffd3d1a3800d82956
Michael Loipführer <michael.loipfuehrer@stusta.de>
2025-04-25 21:30:31 +0200

    fix(core): add missing query for node id in ticket scan check

--- stustapay/core/service/order/order.py
+++ stustapay/core/service/order/order.py
@@ -1231,8 +1231,7 @@ class OrderService(Service[Config]):
             raise InvalidArgument(f"Ticket already has account: {formatted_pins}")
 
         known_pins = await conn.fetch(
-            "select pin from user_tag where pin = any($1)",
-            customer_pins,
+            "select pin from user_tag where pin = any($1) and u.node_id = any($2)", customer_pins, node.ids_to_root
         )
         if len(known_pins) != len(new_ticket_scan.customer_tags):
             unknown_ids = set(customer_pins) - set(i["pin"] for i in known_pins)
