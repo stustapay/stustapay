eb1d9f493c2675e4702f1d41ac37236c6d2a0f62
Michael Loipführer <michael.loipfuehrer@stusta.de>
2026-05-23 17:54:54 +0200

    fix(administration): improve rendering of terminal list in cashier list
    fixes #544

--- web/apps/administration/src/app/routes/cashiers/CashierList.tsx
+++ web/apps/administration/src/app/routes/cashiers/CashierList.tsx
@@ -52,20 +52,28 @@ export const CashierList: React.FC = () => {
     return <Loading />;
   }
 
-  const renderTerminal = (id?: number | null) => {
-    if (!id || !terminals) {
+  const renderTerminals = (ids?: number[] | null) => {
+    if (!ids || !terminals) {
       return "";
     }
-
-    const terminal = selectTerminalById(terminals, id);
-    if (!terminal) {
-      return "";
-    }
+    const actualTerminals = ids
+      .map((id) => selectTerminalById(terminals, id))
+      .filter((t): t is NonNullable<typeof t> => t != null);
+    actualTerminals.sort((a, b) => a.name.localeCompare(b.name));
 
     return (
-      <Link component={RouterLink} key={id} to={TerminalRoutes.detail(terminal.id)}>
-        {terminal.name}
-      </Link>
+      <div>
+        {actualTerminals.map((terminal, index) => {
+          return (
+            <React.Fragment key={terminal.id}>
+              {index > 0 ? ", " : null}
+              <Link component={RouterLink} to={TerminalRoutes.detail(terminal.id)}>
+                {terminal.name}
+              </Link>
+            </React.Fragment>
+          );
+        })}
+      </div>
     );
   };
 
@@ -96,7 +104,7 @@ export const CashierList: React.FC = () => {
       headerName: t("cashier.terminals"),
       flex: 0.5,
       minWidth: 150,
-      renderCell: (params) => params.row.terminal_ids.map((id) => renderTerminal(id)),
+      renderCell: (params) => renderTerminals(params.row.terminal_ids),
     },
     {
       field: "user_tag_id",
