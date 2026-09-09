bc2dde4a45f2e6e98fc7b1c95d09fc650b69df15
TechnoElf <technoelf@undertheprinter.com>
2026-09-09 15:16:59 +0200

    style(core): fix lint

--- stustapay/core/service/terminal.py
+++ stustapay/core/service/terminal.py
@@ -327,7 +327,11 @@ class TerminalService(Service[Config]):
         assert event_node_id is not None
 
         current_token = self.sumup_oauth_cache.get(event_node_id, None)
-        if current_token and current_token.is_valid() and current_token.refresh_token == event_settings.sumup_oauth_refresh_token:
+        if (
+            current_token
+            and current_token.is_valid()
+            and current_token.refresh_token == event_settings.sumup_oauth_refresh_token
+        ):
             return current_token
 
         logger.info(f"Refreshing SumUp Oauth token for event with ID {event_node_id}")
